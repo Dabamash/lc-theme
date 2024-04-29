afc4816fc454517fdc6bf2eef7a66ed7c397f2b5
Dav <dafyddnash@gmail.com>
2024-04-29 23:56:08 +0100

    v1.3.0 Added landing page template

---
+++ header-dlp.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N3VPFKNF');</script>
+<!-- End Google Tag Manager -->
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>
+  <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon-32.png" type="image/x-icon">
+</head>
+<body <?php body_class(); ?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N3VPFKNF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+  <header class="site-header">
+    <div class="container">
+      <a href="<?php echo site_url() ?>" class="float-left">
+        <img src="<?php echo get_template_directory_uri() ?>/images/lc-orange.webp" alt="Latent Clarity Logo" width="200" height="31">
+      </a>
+    </div>
+  </header>
---
+++ landing-page.php
@@ -0,0 +1,34 @@
+<?php
+/*
+Template Name: Landing Page
+*/
+
+include('header-dlp.php'); ?>
+
+<div class="page-banner">
+</div>
+
+<?php
+while (have_posts()) {
+  the_post();
+?>
+
+<div class="container container--narrow page-section">
+  <h1 class="headline--medium t-center"><?php the_title(); ?></h1><br>
+  <?php
+  $theParent = wp_get_post_parent_id(get_the_ID());
+  if ($theParent) { ?>
+    <div>
+      <div><a href="<?php echo get_permalink($theParent); ?>">← back to <?php echo get_the_title($theParent); ?></a></div><hr>
+    </div>
+  <?php } ?>
+
+  <div class="generic-content">
+    <?php the_content(); ?>
+  </div>
+
+</div>
+
+<?php }
+
+?>
