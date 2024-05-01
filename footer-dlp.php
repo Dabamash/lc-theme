0d416975c4bbe9bfcad9c29c6b8ca943ceb0a32a
Dav <dafyddnash@gmail.com>
2024-05-01 12:12:45 +0100

    v1.3.5 Added MC logo/link to DLP footer

--- footer-dlp.php
+++ footer-dlp.php
@@ -1,7 +1,9 @@
 <footer class="site-footer dlp-footer">
     <div class="container">
         <img src="<?php echo get_template_directory_uri() ?>/images/lc-orange-black.webp" alt="Latent Clarity Logo" width="200" height="31">
-        
+        <a href="https://mentorcruise.com/mentor/davnash/">
+            <img src="https://mentorcruise.com/mentor/davnash/widget/" loading="lazy" class="md:h-28" style="max-height: 140px" alt="">
+        </a>
         <div class="footer-links">
             <a href="/terms">Terms of Use</a>
             <a href="/privacy-policy">Privacy Policy</a>
