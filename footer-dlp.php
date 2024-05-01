c68576a42f6e6a94f74912dd1ee9710cd7a0faa4
Dav <dafyddnash@gmail.com>
2024-05-01 12:03:33 +0100

    v1.3.4 Added copyright and terms to DLP footer

--- footer-dlp.php
+++ footer-dlp.php
@@ -1,8 +1,13 @@
 <footer class="site-footer dlp-footer">
-<div class="container">
+    <div class="container">
         <img src="<?php echo get_template_directory_uri() ?>/images/lc-orange-black.webp" alt="Latent Clarity Logo" width="200" height="31">
+        
+        <div class="footer-links">
+            <a href="/terms">Terms of Use</a>
+            <a href="/privacy-policy">Privacy Policy</a>
+        </div>
     </div>
-<p>Copyright Latent Clarity 2024</p>
+    <p>Copyright &copy; <?php echo date('Y'); ?> Latent Clarity</p>
 </footer>
 
 <?php wp_footer(); ?>
